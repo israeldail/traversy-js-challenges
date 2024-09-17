f92753208f44caf1dec7f65c286fc9b4eed3f138
israeldail <israeldail2@gmail.com>
2024-09-17 18:39:02 -0400

    solved title case challenge

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,3 @@
-function titleCase() {}
+const titleCase = (string) => string.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 
 module.exports = titleCase;
