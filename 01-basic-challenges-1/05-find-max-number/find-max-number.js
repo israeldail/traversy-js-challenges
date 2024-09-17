c2c66c2df3819c6f6bcc9f6062b5d0becfb07247
israeldail <israeldail2@gmail.com>
2024-09-16 23:31:55 -0400

    solved find-max-number challenge

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,16 @@
-function findMaxNumber() {}
+function findMaxNumber(array) {
+    let temp;
+    for(let i = 0; i < array.length; i++){
+        for(let j = i+1; j < array.length; j++){
+            if(array[i] < array[j]){
+                temp = array[j];
+            }else if(array[i] === array[j]){
+                temp = array[i];
+            }
+        }
+    }
+
+    return temp;
+}
 
 module.exports = findMaxNumber;
