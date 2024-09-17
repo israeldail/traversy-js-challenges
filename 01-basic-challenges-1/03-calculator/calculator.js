5cda5d90ebae6cda48c4353cae70c78581b041d0
israeldail <israeldail2@gmail.com>
2024-09-16 23:18:55 -0400

    solved calculator challenge

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,13 @@
-function calculator() {}
+function calculator(a,b,operand) {
+    if(operand === "+"){
+        return a+b;
+    }else if(operand === "-"){
+        return a-b;
+    }else if(operand === "*"){
+        return a*b;
+    }else if(operand === "/"){
+        return a/b;
+    }
+}
 
 module.exports = calculator;
