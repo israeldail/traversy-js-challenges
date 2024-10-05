529c20bc9c6f56b8971e2894cd7be3e641865a81
israeldail <israeldail2@gmail.com>
2024-10-05 19:33:56 -0400

    added comments for reference

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,6 +1,9 @@
 function reverseString(string) {
     let reversed = '';
 
+    // i equal to last item in array
+    // i greater than or equal to 0
+    // if true, remove 1 from i until no longer true
     for(let i = string.length - 1; i >= 0; i--){
         reversed += string[i]
     }
