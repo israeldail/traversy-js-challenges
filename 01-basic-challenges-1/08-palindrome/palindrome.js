8e1981e60610971201bb64ac188d102adb0875df
israeldail <israeldail2@gmail.com>
2024-10-05 19:47:45 -0400

    solved palindrome challenge with reg exp

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,11 @@
-function isPalindrome() {}
+function isPalindrome(string) {
+    let reversed = '';
+
+    for(let i = string.length - 1; i >= 0; i--){
+        reversed += string[i]
+    }
+
+    return string.toLowerCase().replace(/[^a-z0-9]/g, '') === reversed.toLowerCase().replace(/[^a-z0-9]/g, '') ? true : false;
+}
 
 module.exports = isPalindrome;
