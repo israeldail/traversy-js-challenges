368be03e80cc62e5d88b8f47934873e6cb063c93
israeldail <israeldail2@gmail.com>
2024-10-05 22:13:06 -0400

    solved fizzbuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(n) {
+    let fizzBuzzArr = [];
+
+    for(let i = 1; i <= n; i++){
+        if(i % 15 === 0){
+            fizzBuzzArr.push('FizzBuzz');
+        }else if(i % 5 === 0) {
+            fizzBuzzArr.push('Buzz');
+        }else if(i % 3 === 0) {
+            fizzBuzzArr.push('Fizz');
+        }else {
+            fizzBuzzArr.push(i);
+        }
+    }
+
+    return fizzBuzzArr;
+}
 
 module.exports = fizzBuzzArray;
