41957e90105f8d3af77a325cd74c042e53c02ff0
israeldail <israeldail2@gmail.com>
2024-10-05 20:06:23 -0400

    solved vowel count

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,16 @@
-function countVowels() {}
+function countVowels(str) {
+    let vowels = ['a','e','i','o','u'];
+    let count = 0;
+
+    for(let i = 0; i < str.length; i++){
+        for(let j = 0; j < vowels.length; j++){
+            if(str.toLowerCase()[i] === vowels[j]){
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
 
 module.exports = countVowels;
