ee6e3041fb89b54b32278d7a882d26d47132b96a
israeldail <israeldail2@gmail.com>
2024-10-07 23:22:42 -0400

    solved missing num challenge

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,13 @@
-function findMissingNumber() {}
+function findMissingNumber(nums) {
+    let missingNum;
+
+    for(let i = 1; i <= nums.length; i++) {
+        if(!nums.includes(i)){
+            missingNum = i;
+        }
+    }
+
+    return missingNum;
+}
 
 module.exports = findMissingNumber;
