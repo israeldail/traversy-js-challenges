57439d49a60211cc36796d931ad60ee379e60edd
israeldail <israeldail2@gmail.com>
2024-11-08 20:52:14 -0500

    solved phone number format challenge

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,8 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(phoneNum) {
+    const firstThree = Array.from(phoneNum.slice(0,3))
+    const secondThree = Array.from(phoneNum.slice(3,6))
+    const lastFour = Array.from(phoneNum.slice(-4))
+    return `(${firstThree.join('')}) ${secondThree.join('')}-${lastFour.join('')}`
+}
 
 module.exports = formatPhoneNumber;
