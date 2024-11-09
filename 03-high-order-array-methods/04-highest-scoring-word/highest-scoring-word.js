fe1d3002ef319475e1ec93c844c7581cb21c1ac9
israeldail <israeldail2@gmail.com>
2024-11-09 00:29:25 -0500

    added comments to challenge for tomorrow

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,9 @@
-function highestScoringWord() {}
+function highestScoringWord(string) {
+    // TODO: Use the split method to turn the string into an array, use split(" ") to separate whole words
+
+    // TODO: Use the map method to calculate score for each word. Might have to google a part of this.
+
+    // TODO: Use reduce method to find word with highest score and return that.
+}
 
 module.exports = highestScoringWord;
