3b68128831d56610301d572d62f388ce9f93a55f
israeldail <israeldail2@gmail.com>
2024-11-08 19:27:54 -0500

    solved unique chars challenge

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,13 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(string) {
+    for(let i = 0; i < string.length; i++){
+        for(let j = i+1; j < string.length; j++){
+            if(string[i] === string[j]){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
 
 module.exports = areAllCharactersUnique;
