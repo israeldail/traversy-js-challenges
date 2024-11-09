db6fb41be18bf541dba4d5d2443c11a9177df11a
israeldail <israeldail2@gmail.com>
2024-11-09 13:42:15 -0500

    solved anagram challenge

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams-run.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams-run.js
@@ -1,5 +1,8 @@
 const validAnagrams = require('./valid-anagrams');
 
 const result = validAnagrams('app', 'ppa');
+const result2 = validAnagrams('aaa', 'aaab');
+
 
 console.log(result);
+console.log(result2);
--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,19 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+    if (str1.length !== str2.length) return false; // Check lengths first
+    // counting the occurrence of each letter
+    const str1Count = str1.split('').reduce((acc, char) => {
+        acc[char] = (acc[char] || 0) + 1;
+        return acc;
+      }, {});
+    
+      const str2Count = str2.split('').reduce((acc, char) => {
+        acc[char] = (acc[char] || 0) + 1;
+        return acc;
+      }, {});
+
+    return Object.keys(str1Count).every(
+        (char) => str1Count[char] === str2Count[char]
+      );
+}
 
 module.exports = validAnagrams;
