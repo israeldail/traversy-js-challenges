3631b260545fb6f0872e48ced4f06313a94398c7
israeldail <israeldail2@gmail.com>
2024-11-09 15:01:25 -0500

    solved hashtag generator challenge

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,8 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+    if(str.length > 140 || str.length === 0 || str === " ") return false;
+
+    const modifiedStr = str.split(" ").map((word) => word.slice(0,1).toUpperCase() + word.slice(1)).join("").trim();
+    return "#" + modifiedStr;
+}
 
 module.exports = generateHashtag;
