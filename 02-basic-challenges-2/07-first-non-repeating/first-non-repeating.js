a969707cba267c0033c4c61c96d6419063620ba4
israeldail <israeldail2@gmail.com>
2024-11-08 20:04:41 -0500

    solved first non repeating challenge

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,18 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+    const charCount = {};
+
+    for(const char of str) {
+        // if it has a value add 1, if not add 1 starting from 0
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for(const char of str) {
+        if(charCount[char] === 1){
+            return char;
+        }
+    }
+
+    return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
