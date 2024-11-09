ea2ade4e47071a076353ce2b77d528d69f2c2bae
israeldail <israeldail2@gmail.com>
2024-11-09 00:23:08 -0500

    small change

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -3,13 +3,13 @@ const products = [
     { name: 'Chips', price: 1.2, quantity: 10 },
     { name: 'Soda', price: 1.0, quantity: 8 },
     { name: 'Candy', price: 0.5, quantity: 15 },
-];
+  ];
 
 function calculateTotalSalesWithTax(products, taxRate) {
     const totalSales = products.reduce((sum, product) => sum + product.price * product.quantity, 0);
-    const taxAmount = (totalSales * taxRate) / 100;
-    const totalSalesWithTax = totalSales + taxAmount;
-    return totalSalesWithTax;
+    const taxes = (totalSales * taxRate) / 100;
+    const total = totalSales + taxes;
+    return total;
 }
 
 module.exports = calculateTotalSalesWithTax;
