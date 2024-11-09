578f6e952267cbbca2356108f5a962eb08a11c4c
israeldail <israeldail2@gmail.com>
2024-11-09 15:17:41 -0500

    solved valid ip4 challenge

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,9 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+    let sum = 0;
+    for(octet in str.split(".")) {
+        sum += 1;
+    }
+    return str.split(".").every((octet) => octet >= 0 && octet <= 255 && sum === 4 && !octet.startsWith(0) );
+};
 
 module.exports = isValidIPv4;
