ad0753b90398ef4dc5b0a8b3c6108a2b0fa593bf
israeldail <israeldail2@gmail.com>
2024-11-09 10:58:03 -0500

    solved highest scoring word challenge

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,9 +1,23 @@
 function highestScoringWord(string) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const wordsWithScores = {};
     // TODO: Use the split method to turn the string into an array, use split(" ") to separate whole words
-
+    const wordArr = string.split(" ");
     // TODO: Use the map method to calculate score for each word. Might have to google a part of this.
-
+    const wordScores = wordArr.map((word) => {
+        let sum = 0;
+        for(letter of word) {
+            let alphabetIndex = alphabet.indexOf(letter) + 1;
+            sum += alphabetIndex;
+            wordsWithScores[word] = sum;
+        }
+    });
     // TODO: Use reduce method to find word with highest score and return that.
+    const highestScoreWord = Object.keys(wordsWithScores).reduce((maxWord, key) => {
+        return wordsWithScores[key] > wordsWithScores[maxWord] ? key : maxWord;
+    }, Object.keys(wordsWithScores)[0]);
+
+    return highestScoreWord;
 }
 
 module.exports = highestScoringWord;
