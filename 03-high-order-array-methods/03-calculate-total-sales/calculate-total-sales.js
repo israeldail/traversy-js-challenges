b155a2a6af30cf1808f28a6cc6146754f94229d2
israeldail <israeldail2@gmail.com>
2024-11-09 00:21:03 -0500

    solved calculate total sales challenge

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,15 @@
-function calculateTotalSalesWithTax() {}
+const products = [
+    { name: 'Chocolate', price: 2.5, quantity: 5 },
+    { name: 'Chips', price: 1.2, quantity: 10 },
+    { name: 'Soda', price: 1.0, quantity: 8 },
+    { name: 'Candy', price: 0.5, quantity: 15 },
+];
+
+function calculateTotalSalesWithTax(products, taxRate) {
+    const totalSales = products.reduce((sum, product) => sum + product.price * product.quantity, 0);
+    const taxAmount = (totalSales * taxRate) / 100;
+    const totalSalesWithTax = totalSales + taxAmount;
+    return totalSalesWithTax;
+}
 
 module.exports = calculateTotalSalesWithTax;
