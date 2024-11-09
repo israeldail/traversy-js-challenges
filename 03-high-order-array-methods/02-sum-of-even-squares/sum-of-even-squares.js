7b91826dfb46198c027a48ab99643a3f33bd785d
israeldail <israeldail2@gmail.com>
2024-11-08 22:03:30 -0500

    solved sum of even squares challenge

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+    return arr
+    .filter((item) => item % 2 === 0)
+    .map((item) => item ** 2)
+    .reduce((current, acc) => current+ acc, 0)
+}
 
 module.exports = sumOfEvenSquares;
