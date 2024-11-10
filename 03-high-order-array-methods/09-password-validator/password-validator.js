62da864237eab6cf94f41d0ad2d78dce2d5f04c6
israeldail <israeldail2@gmail.com>
2024-11-10 11:10:37 -0500

    solved password validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,6 @@
-function validatePassword() {}
+function validatePassword(password) {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{8,}/g;
+    return regex.test(password);
+}
 
 module.exports = validatePassword;
