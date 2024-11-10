05e9d28c12603df535bdb3a14db1fc8023c0cef1
israeldail <israeldail2@gmail.com>
2024-11-10 11:23:07 -0500

    another comment

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,5 +1,6 @@
 function validatePassword(password) {
     // ?=.* is a look ahead, making things required
+    // ^ starts with
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{8,}/g;
     return regex.test(password);
 }
