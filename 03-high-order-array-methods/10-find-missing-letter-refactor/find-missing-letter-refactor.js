d563782bccd665d79707019a0bada12917f6c542
israeldail <israeldail2@gmail.com>
2024-11-10 19:37:43 -0500

    comments

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,12 +1,15 @@
 function findMissingLetter(arr) {
+    // char code for first letter
     let start = arr[0].charCodeAt(0);
+    // variable for missing character code
     const missingCharCode = arr
-        .map((char) => char.charCodeAt(0))
+        .map((char) => char.charCodeAt(0)) // get the character code for each letter
         .find((current) => {
+            // if the current code subtracted from the start code is greater than one return true
             if(current - start > 1) {
                 return true;
             }
-
+            // if not make the start code the current code until the result is greater than one
             start = current;
             return false;
         });
