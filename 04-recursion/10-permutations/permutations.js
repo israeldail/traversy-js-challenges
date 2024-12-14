0d19d370349a8aceacf830ddfe69a6172197702d
israeldail <israeldail2@gmail.com>
2024-12-14 15:30:10 -0500

    comment

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,11 +1,16 @@
 function permutations(str) {
   const result = [];
-
+  // base case 
   if (str.length === 0) {
     result.push('');
     return result;
   }
-
+  // iterate over string 
+  // get first letter 
+  // get rest of string 
+  // get subPermutations 
+  // iterate over subPermutations
+  // push result to array
   for (let i = 0; i < str.length; i++) {
     const firstChar = str[i];
     const restOfString = str.slice(0, i) + str.slice(i + 1);
