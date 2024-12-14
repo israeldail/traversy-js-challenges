1ced5442ec531466733b3e993ba26bacf8e95791
israeldail <israeldail2@gmail.com>
2024-12-14 12:14:58 -0500

    solved reverseString recursive

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+ if(str.length <= 1) {
+    return str;
+  } 
+
+  return reverseString(str.slice(1)) + str[0];
+}
 
 module.exports = reverseString;
